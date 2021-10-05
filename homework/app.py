046d1ed009f3a209df45dd201bc41558ff8ef93a
kimcno3 <77563468+kimcno3@users.noreply.github.com>
2021-10-06 02:25:54 +0900

    4주차 숙제 완성

---
+++ homework/app.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, jsonify, request
+
+app = Flask(__name__)
+
+from pymongo import MongoClient
+
+client = MongoClient('localhost', 27017)
+db = client.dbhomework
+
+
+## HTML 화면 보여주기
+@app.route('/')
+def homework():
+    return render_template('index.html')
+
+
+# 주문하기(POST) API
+@app.route('/order', methods=['POST'])
+def save_order():
+    name_receive = request.form['name_give']
+    count_receive = request.form['count_give']
+    address_receive = request.form['address_give']
+    phonenumber_receive = request.form['phonenumber_give']
+
+    doc = {
+        'name' : name_receive,
+        'count' : count_receive,
+        'address' : address_receive,
+        'phonenumber' : phonenumber_receive
+    }
+    db.homework.insert_one(doc)
+
+    return jsonify({'msg': '주문 완료!!'})
+
+
+# 주문 목록보기(Read) API
+@app.route('/order', methods=['GET'])
+def view_orders():
+    orders = list(db.homework.find({}, {'_id': False}))
+    return jsonify({'order_list': orders})
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
