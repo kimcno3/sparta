ed60731a432aaabf9f18f70d9874c6b6d9b20258
kimcno3 <77563468+kimcno3@users.noreply.github.com>
2021-10-27 16:47:33 +0900

    변수 이름 변경
    - phonenuber >> phoneNumber

--- homework/app.py
+++ homework/app.py
@@ -9,11 +9,10 @@ client = MongoClient('mongodb://test:test@localhost', 27017)
 db = client.dbhomework
 
 
-## HTML 화면 보여주기
+# HTML 화면 보여주기
 @app.route('/')
 def homework():
     return render_template('index.html')
-
 
 # 주문하기(POST) API
 @app.route('/order', methods=['POST'])
@@ -21,26 +20,23 @@ def save_order():
     name_receive = request.form['name_give']
     count_receive = request.form['count_give']
     address_receive = request.form['address_give']
-    phonenumber_receive = request.form['phonenumber_give']
-
+    phoneNumber_receive = request.form['phoneNumber_give']
 
     doc = {
         'name' : name_receive,
         'count' : count_receive,
         'address' : address_receive,
-        'phonenumber' : phonenumber_receive
+        'phoneNumber' : phoneNumber_receive
     }
     db.homework.insert_one(doc)
 
     return jsonify({'msg': '주문 완료!!'})
-
 
 # 주문 목록보기(Read) API
 @app.route('/order', methods=['GET'])
 def view_orders():
     orders = list(db.homework.find({}, {'_id': False}))
     return jsonify({'order_list': orders})
-
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
